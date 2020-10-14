7c2ba0aa390d24fc1b85b5d7fbc0439f0d235f50
Thiago Nascimento <thiagodnf@gmail.com>
2020-10-13 21:11:53 -0400

    Create Address.java

---
+++ src/main/java/edu/iselab/grocery/persistence/model/Address.java
@@ -0,0 +1,13 @@
+package edu.iselab.grocery.persistence.model;
+
+public class Address {
+    
+    public String street;
+    
+    private String city;
+    
+    private String state;
+    
+    private String zipCode;
+
+}
