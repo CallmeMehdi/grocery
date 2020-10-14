f4167c75cb8a276cbf92337095a9cf376403350b
Thiago Nascimento <thiagodnf@gmail.com>
2020-10-13 20:55:52 -0400

    added first methods for product repository

--- src/main/java/edu/iselab/grocery/persistence/repository/ProductRepository.java
+++ src/main/java/edu/iselab/grocery/persistence/repository/ProductRepository.java
@@ -1,5 +1,19 @@
 package edu.iselab.grocery.persistence.repository;
 
-public class ProductRepository {
+import java.util.ArrayList;
+import java.util.List;
 
+import edu.iselab.grocery.persistence.model.Product;
+
+public class ProductRepository {
+    
+    private List<Product> products;
+    
+    public ProductRepository() {
+        this.products = new ArrayList<>();
+    }
+    
+    public List<Product> findAll(){
+        return products;
+    }
 }
