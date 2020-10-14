3a04f90b67a4c575dbda52379a4eb2165b8f4050
Thiago Nascimento <thiagodnf@gmail.com>
2020-10-14 00:16:57 -0400

    added phonenumber

--- src/main/java/edu/iselab/grocery/persistence/model/Customer.java
+++ src/main/java/edu/iselab/grocery/persistence/model/Customer.java
@@ -3,6 +3,8 @@ package edu.iselab.grocery.persistence.model;
 public class Customer extends AbstractModel {
 
     private String fullName;
+    
+    public String phoneNumber;
 
     public String getFullName() {
         return fullName;
