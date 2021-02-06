d5b624aa129fb8a9e83127ed08fed6c7a4ce506f
Thiago Ferreira <thiagodnf@gmail.com>
2021-02-05 22:45:00 -0500

    Create Class1.java

---
+++ src/main/java/edu/iselab/grocery/random/Class1.java
@@ -0,0 +1,14 @@
+package edu.iselab.grocery.random;
+
+import edu.iselab.grocery.persistence.model.Product;
+
+public class Class1 {
+    
+    private Product p;
+    
+    public void execute() {
+        
+        p.price = 2;
+    }
+
+}
