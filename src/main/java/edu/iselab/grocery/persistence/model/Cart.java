8e42ae22f7d80f4a8a898fe60b062c0b3c0053b9
Thiago Ferreira <thiagodnf@gmail.com>
2021-02-07 10:20:07 -0500

    Update Cart.java

--- src/main/java/edu/iselab/grocery/persistence/model/Cart.java
+++ src/main/java/edu/iselab/grocery/persistence/model/Cart.java
@@ -2,7 +2,7 @@ package edu.iselab.grocery.persistence.model;
 
 import java.util.ArrayList;
 
-public class Cart extends ArrayList<Product>{
+public class Cart extends ArrayList<Product> {
 
     private static final long serialVersionUID = 4613077300046185369L;
 
